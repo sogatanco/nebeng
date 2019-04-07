eba5d47eaca51b05c73f4a40bd28467a1a73fcfc
sogatanco <adeliapucha46@gmail.com>
2019-04-08 00:11:54 +0700

    edit login model

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -3,7 +3,7 @@
 class Login_model extends CI_Model
 {
     public function cekLogin($email, $pass){
-        $resultLogin=$this->db->get_where('user',['us_email'=>$email, 'us_password'=>$pass]);
+        $resultLogin=$this->db->get_where('user',['us_email'=>$email, 'us_password'=>$pass, 'us_level'=>1]);
         if($resultLogin->num_rows()>0){
             $token=$resultLogin->row_array();
             return $token['us_token'];
