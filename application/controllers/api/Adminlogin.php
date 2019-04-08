2ac44be9352bca96007935eb5ccb9eeab5a7e2fc
sogatanco <adeliapucha46@gmail.com>
2019-04-08 22:41:10 +0700

    finished admin login function

--- application/controllers/api/Adminlogin.php
+++ application/controllers/api/Adminlogin.php
@@ -15,11 +15,26 @@ class Adminlogin extends REST_Controller
         $this->load->model('Adminlogin_model', 'admin');
     }
 
-    public function login_post(){
+    public function index_post(){
         $username=$this->post('username');
         $pass=$this->post('pass');
 
-        if($this->admin->cek_login($username, $pass)==TRUE){
+        if($this->admin->cek_login($username, $pass)>0){
+            $datatoken=[
+                'username'=>$username,
+                'level'=>0,
+                'lastLogin'=>date("Y-m-d h:i:sa"),
+                'ipaddress'=>$this->input->ip_address()
+            ];
+            $token=$this->objOfJwt->GenerateToken($datatoken);
+            $this->admin->updateToken($token, $username);
+            $this->response([
+                'status' => true,
+                'token' => $token,
+                'data'=>$datatoken
+            ], REST_Controller::HTTP_OK); 
+        }
+        else{
             $this->response([
                 'status' => false,
                 'message' => 'username and password not match'
--- application/models/Adminlogin_model.php
+++ application/models/Adminlogin_model.php
@@ -4,6 +4,11 @@ class Adminlogin_model extends CI_Model
     public function cek_login($username, $pass)
     {
         $this->db->get_where('user', ['us_email'=>$username,'us_password'=>$pass, 'us_level'=>0]);
-        return TRUE;
+        return $this->db->affected_rows();
+    }
+
+    public function updateToken($token, $username)
+    {
+        $this->db->update('user',['us_token'=>$token], ['us_email'=>$username]);
     }
 }
