5c1931c5dc2489b1ba1032f23b820bd9b8a831ce
sogatanco <adeliapucha46@gmail.com>
2019-06-15 19:24:47 +0700

    edited welcome page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -75,11 +75,7 @@
 
     <div id="body">
 
-        <h2><a href="<?php echo site_url('rest-server'); ?>">REST Server Tests</a></h2>
-
-        <?php if (file_exists(FCPATH.'documentation/index.html')) : ?>
-        <h2><a href="<?php echo base_url('documentation/index.html'); ?>" target="_blank">REST Server Documentation</a></h2>
-        <?php endif ?>
+        <h2><a href="https://github.com/sogatanco/nebeng/blob/master/README.md">REST Server Documentation</a></h2>
 
         <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
@@ -89,9 +85,6 @@
         <p>The corresponding controller for this page is found at:</p>
         <code>application/controllers/Welcome.php</code>
 
-        <?php if (file_exists(FCPATH.'user_guide/index.html')) : ?>
-        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="<?php echo base_url('user_guide/index.html'); ?>" target="_blank">User Guide</a>.</p>
-        <?php endif ?>
     </div>
 
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '' ?></p>
