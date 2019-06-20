624d673c48ee2774a795066de79775818e166abe
sogatanco <adeliapucha46@gmail.com>
2019-06-20 23:03:36 +0700

    called api to approve mechanic place(bengkel)

--- asset/js/unapprove.js
+++ asset/js/unapprove.js
@@ -1,50 +1,57 @@
 $(document).ready(function(){
-    $.ajax({
-        type:"get",
-        url:"../../api/admin/viewbengkel?token="+Cookies.get("token"),
-        success: function(response){
-            $(".junapprove").html(response.data.length);
-            popular=response.data;
-            console.log(popular)
-            for(i=0;i<popular.length;i++){
-                judul=popular[i].bk_namabengkel;
-                deskripsi=popular[i].bk_deskripsi;
-                rating=popular[i].total_rating/popular[i].j_ulasan;
-                rating = rating || 0
-                rating=rating.toFixed(1)
-                if(deskripsi==""){
-                    deskripsi="Description";
-                }
-                kategori=popular[i].bk_kategori;
-                if(kategori=1){
-                    kategori="<span class='fas fa-motorcycle text-danger'></span> Motor";
-                }
-                else{
-                    kategori="<span class='fas fa-car text-danger'></span> Mobil";
-                }
-                $("#content").append(`
-                <div class="col-sm-4">
-                    <div class="card-view" data-toggle="modal" data-target="#myModal" data-id="`+popular[i].bk_id+`" data-lat="`+popular[i].bk_lat+`" data-long="`+popular[i].bk_long+`" data-judul="`+judul+`" data-rating="`+rating+`" data-ulasan="`+popular[i].j_ulasan+`" data-kategori="`+kategori+`" data-desk="`+deskripsi+`" data-lay="`+popular[i].bk_layanan+`" data-pemilik="`+popular[i].bk_pemilik+`" data-tanggal="`+popular[i].bk_startdate+`" data-tlp="`+popular[i].bk_telpon+`" data-waktu="`+popular[i].bk_availabletime+`">
-                        <div class="row">
-                            <div class="col-6">
-                                <img src="../../asset/images/`+popular[i].bk_foto+`" class="gambar">
+    getContent();
+    function getContent(){
+        $.ajax({
+            type:"get",
+            url:"../../api/admin/viewbengkel?token="+Cookies.get("token"),
+            success: function(response){
+                $(".junapprove").html(response.data.length);
+                $("#content").html("")
+                popular=response.data;
+                console.log(popular)
+                for(i=0;i<popular.length;i++){
+                    judul=popular[i].bk_namabengkel;
+                    deskripsi=popular[i].bk_deskripsi;
+                    rating=popular[i].total_rating/popular[i].j_ulasan;
+                    rating = rating || 0
+                    rating=rating.toFixed(1)
+                    if(deskripsi==""){
+                        deskripsi="Description";
+                    }
+                    kategori=popular[i].bk_kategori;
+                    if(kategori=1){
+                        kategori="<span class='fas fa-motorcycle text-danger'></span> Motor";
+                    }
+                    else{
+                        kategori="<span class='fas fa-car text-danger'></span> Mobil";
+                    }
+                    $("#content").append(`
+                    <div class="col-sm-4">
+                        <div class="card-view" data-toggle="modal" data-target="#myModal" data-id="`+popular[i].bk_id+`" data-lat="`+popular[i].bk_lat+`" data-long="`+popular[i].bk_long+`" data-judul="`+judul+`" data-rating="`+rating+`" data-ulasan="`+popular[i].j_ulasan+`" data-kategori="`+kategori+`" data-desk="`+deskripsi+`" data-lay="`+popular[i].bk_layanan+`" data-pemilik="`+popular[i].bk_pemilik+`" data-tanggal="`+popular[i].bk_startdate+`" data-tlp="`+popular[i].bk_telpon+`" data-waktu="`+popular[i].bk_availabletime+`">
+                            <div class="row">
+                                <div class="col-6">
+                                    <img src="../../asset/images/`+popular[i].bk_foto+`" class="gambar">
+                                </div>
+                                <div class="col-6 content">
+                                <strong class="title">`+judul.substring(0,14)+`</strong>
+                                    <small class="text-muted">
+                                        <p><span class="badge badge-pill badge-warning notification" >`+rating+` </span>  `+popular[i].j_ulasan+` ulasan | `+kategori+`</p>
+                                        <p>`+deskripsi.substring(0,35)+`...</p>
+                                    </small> 
+                                </div>
                             </div>
-                            <div class="col-6 content">
-                            <strong class="title">`+judul.substring(0,14)+`</strong>
-                                <small class="text-muted">
-                                    <p><span class="badge badge-pill badge-warning notification" >`+rating+` </span>  `+popular[i].j_ulasan+` ulasan | `+kategori+`</p>
-                                    <p>`+deskripsi.substring(0,35)+`...</p>
-                                </small> 
-                            </div>
+    
                         </div>
-
                     </div>
-                </div>
-                `);
+                    `);
+                }
+            },
+            error:function(err){
+                $("#content").html("")
+                $(".junapprove").html(0);
             }
-        }
-    });
-
+        }); 
+    }
 
 // get data when opened modal
     $('#content').on( "click",".card-view", function() {
@@ -84,8 +91,26 @@ $(document).ready(function(){
             alert("delete bengkel with: "+$(this).data('idbengkel'))
         })
         $("#terima").click(function(){
-            alert("approve bengkel with: "+$(this).data('idbengkel'))
-        })
+            var conf=confirm("Are you sure to Approve ?");
+            if(conf==true){
+                $.ajax({
+                    type:"PUT",
+                    url:"../../api/admin/approvebengkel",
+                    data:{
+                        token:Cookies.get('token'),
+                        idbengkel:$(this).data('idbengkel')
+                    },
+                    success:function(response){
+                        getContent()
+                        alert("approved")
+                        $("#myModal").modal("toggle");
+                    },
+                    error:function(err){
+                        alert("approve failed")
+                    }
+                });
+            } 
+        });
     });
 
 // initialize map
@@ -125,7 +150,6 @@ $(document).ready(function(){
             day_diff < 7 && day_diff + " days ago" ||
             day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
     }
-    
     
   
 });
