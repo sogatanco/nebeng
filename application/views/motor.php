ab5dd80f8e4c51111f42fe48d892c169930387dd
sogatanco <adeliapucha46@gmail.com>
2019-06-20 23:59:44 +0700

    added detail in car and motorcycle mechanic vie

--- application/views/motor.php
+++ application/views/motor.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="<?php echo base_url()?>asset/css/bengkel.css" rel="stylesheet">
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALisurDb7RVJUKs2Es67Tw2jQL55LoClw"></script>
     <?php $this->load->view('src') ?>
     <title>Nebeng-Admin | Bengkel Motor </title>
 </head>
@@ -22,6 +23,42 @@
             <div class="row" id="content">
                 <!-- data from js -->
             </div>
+
+
+
+            <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg" >
+      <div class="modal-content">
+        <div id="map"></div>
+        <!-- in Jquery -->
+        <div id="detailbengkel">
+            <div class="row">
+                <div class="col-8">
+                    <h3 id="judul" class="judul"></h3>
+                    <p class="text-muted" id="attribute"></p>
+                </div>
+                <div class="col-4">
+                    <div class="float-right" id="tombol">
+                        <div class="terima"></div>
+                        <div class="delete"></div>
+                    </div>    
+                </div>
+            </div>
+            
+            <hr>
+            <p class="text-justify" id="deskripsi"></p>
+            <p class="text-justify" id="layanan"></p>
+            <br>
+            <h5 class="judul">Ulasan</h5>
+                <hr>
+            <div id="komentars">
+                     <!--  data from jquery-->
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
         </div>
 
--- asset/js/motor.js
+++ asset/js/motor.js
@@ -1,50 +1,54 @@
 $(document).ready(function(){
-    $.ajax({
-        type:"get",
-        url:"../../../api/general/bengkel?token=1234567&kategori=1",
-        success: function(response){
-            popular=response.data.sort(function(a,b){
-                return b.j_ulasan - a.j_ulasan;
-            });
-            console.log(popular)
-            for(i=0;i<popular.length;i++){
-                judul=popular[i].bk_namabengkel;
-                deskripsi=popular[i].bk_deskripsi;
-                rating=popular[i].total_rating/popular[i].j_ulasan;
-                rating = rating || 0
-                rating=rating.toFixed(1)
-                if(deskripsi==""){
-                    deskripsi="Description";
-                }
-                kategori=popular[i].bk_kategori;
-                if(kategori=1){
-                    kategori="Motor"
-                }
-                else{
-                    kategori="Mobil"
-                }
-                $("#content").append(`
-                <div class="col-sm-4">
-                    <div class="card-view">
-                        <div class="row">
-                            <div class="col-6">
-                                <img src="../../../asset/images/`+popular[i].bk_foto+`" class="gambar">
+    getContent();
+    function getContent(){
+        $("#content").html("")
+        $.ajax({
+            type:"get",
+            url:"../../../api/general/bengkel?token=1234567&kategori=1",
+            success: function(response){
+                popular=response.data.sort(function(a,b){
+                    return b.j_ulasan - a.j_ulasan;
+                });
+                console.log(popular)
+                for(i=0;i<popular.length;i++){
+                    judul=popular[i].bk_namabengkel;
+                    deskripsi=popular[i].bk_deskripsi;
+                    rating=popular[i].total_rating/popular[i].j_ulasan;
+                    rating = rating || 0
+                    rating=rating.toFixed(1)
+                    if(deskripsi==""){
+                        deskripsi="Description";
+                    }
+                    kategori=popular[i].bk_kategori;
+                    if(kategori=1){
+                        kategori="Motor"
+                    }
+                    else{
+                        kategori="Mobil"
+                    }
+                    $("#content").append(`
+                    <div class="col-sm-4">
+                        <div class="card-view" data-toggle="modal" data-target="#myModal" data-id="`+popular[i].bk_id+`" data-lat="`+popular[i].bk_lat+`" data-long="`+popular[i].bk_long+`" data-judul="`+judul+`" data-rating="`+rating+`" data-ulasan="`+popular[i].j_ulasan+`" data-kategori="`+kategori+`" data-desk="`+deskripsi+`" data-lay="`+popular[i].bk_layanan+`" data-pemilik="`+popular[i].bk_pemilik+`" data-tanggal="`+popular[i].bk_startdate+`" data-tlp="`+popular[i].bk_telpon+`" data-waktu="`+popular[i].bk_availabletime+`">
+                            <div class="row">
+                                <div class="col-6">
+                                    <img src="../../../asset/images/`+popular[i].bk_foto+`" class="gambar">
+                                </div>
+                                <div class="col-6 content">
+                                <strong class="title">`+judul.substring(0,14)+`</strong>
+                                    <small class="text-muted">
+                                        <p><span class="badge badge-pill badge-warning notification" >`+rating+` </span>  `+popular[i].j_ulasan+` ulasan | `+kategori+`</p>
+                                        <p>`+deskripsi.substring(0,35)+`...</p>
+                                    </small> 
+                                </div>
                             </div>
-                            <div class="col-6 content">
-                            <strong class="title">`+judul.substring(0,14)+`</strong>
-                                <small class="text-muted">
-                                    <p><span class="badge badge-pill badge-warning notification" >`+rating+` </span>  `+popular[i].j_ulasan+` ulasan | `+kategori+`</p>
-                                    <p>`+deskripsi.substring(0,35)+`...</p>
-                                </small> 
-                            </div>
+    
                         </div>
-
                     </div>
-                </div>
-                `);
+                    `);
+                }
             }
-        }
-    });
+        });
+    }
 
     // unapprove
     $.ajax({
@@ -54,4 +58,100 @@ $(document).ready(function(){
             $(".junapprove").html(response.data.length);
         }
     });
+
+    $('#content').on( "click",".card-view", function() {
+        initialize(new google.maps.LatLng($(this).data('lat'), $(this).data('long')));
+        $('#judul').html(ca($(this).data('judul')));
+        $('#tombol').html(`
+        <button type="button" class="btn btn-danger" id="delete" data-idbengkel="`+$(this).data('id')+`"><i class='fas fa-trash-alt'></i></button>
+        `)
+        $('#attribute').html(`<span class="badge badge-pill badge-warning notification" >`+$(this).data('rating')+`</span> `+$(this).data('ulasan')+` ulasan | `+$(this).data('kategori'));
+        $("#deskripsi").html($(this).data('desk')+`.`);
+        $("#layanan").html(`Bengkel ini ditambahkan oleh <b>`+$(this).data('pemilik')+ `</b> sejak `+$(this).data('tanggal')+ `. Adapun layanan yang bisa didapatkan di sini antara lain :` +$(this).data('lay')+` dengan waktu pengoprasian mulai dari ` +$(this).data('waktu')+` WIB. Untuk informasi lebih lanjut silakan menghubungi langsung `+$(this).data('tlp')+`.`);
+        $.ajax({
+            type:"get",
+            url:"../../../api/general/ulasan?token=1234567&idbengkel="+$(this).data('id'),
+            success:function(respon){
+                $('#komentars').html('');
+                $.each(respon.data,  function(i,d){
+                    bintang=parseFloat(d.ul_rating)
+                    bintang=bintang.toFixed(1)
+                    $('#komentars').append(`
+                    <p><b>`+d.ul_user+`</b><br><small class="text-muted">Rated  <span class="badge badge-pill badge-info notification" >  `+bintang+`</span> | `+prettyDate(d.ul_time)+` </small><br>`+d.ul_ulasan+`</p>
+                    `)
+                })
+                console.log(respon.data)
+            },
+            error:function(error){
+                $('#komentars').html(`<p>Maaf belum ada data</p>`)
+            }
+            
+        });
+        
+    });
+
+    $('#myModal').on('shown.bs.modal', function(e) {
+        $("#delete").click(function(){
+            var conf=confirm("Are you sure to Delete it ?");
+            if(conf==true){
+                console.log($(this).data('idbengkel'))
+                $.ajax({
+                    type:"DELETE",
+                    url:"../../../api/admin/deletebengkel",
+                    data:{
+                        token:Cookies.get('token'),
+                        id:$(this).data('idbengkel')
+                    },
+                    success:function(res){
+                        alert("Deleted")
+                        $("#myModal").modal("toggle");
+                        getContent()
+                    },
+                    error:function(err){
+                        alert("failed to delete")
+                    }
+                })
+            }
+        })
+    });
+
+// initialize map
+    function initialize(myCenter) {
+        var marker = new google.maps.Marker({
+            position: myCenter
+        });
+      var mapProp = {
+            center: myCenter,
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map"), mapProp);
+        marker.setMap(map);
+    };
+
+    // uppercase in first strring
+    function ca(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // convert date to pretty 
+    function prettyDate(time){
+        var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+            diff = (((new Date()).getTime() - date.getTime()) / 1000),
+            day_diff = Math.floor(diff / 86400);
+                
+        if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+            return;       
+        return day_diff == 0 && (
+                diff < 60 && "just now" ||
+                diff < 120 && "1 minute ago" ||
+                diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+                diff < 7200 && "1 hour ago" ||
+                diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+            day_diff == 1 && "Yesterday" ||
+            day_diff < 7 && day_diff + " days ago" ||
+            day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+    }
+    
+
 });
